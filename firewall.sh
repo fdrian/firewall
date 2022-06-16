09ac9a28ddeec29c0cb23c5fc21e922a8eb851e3
Adriano Freitas <kryptic@adrianofreitas.me>
2022-06-16 17:23:18 -0400

    Simple firewall

---
+++ firewall.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+## Adriano Freitas - 2022
+## adrinofreitas.me
+## Simple firewall with knockd
+
+# Clear rules
+iptables -F
+iptables -X
+
+# Default policy
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptabels -P OUTPUT ACCEPT
+
+# Reject 
+# iptables -A INPUT -i eth0 -p tcp --dport 80 -j REJECT
+
+# Rules
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -j DROP
+
+
+
