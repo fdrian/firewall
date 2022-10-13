2cc83d6a1b16d4eae3d195b534a2cd7bc928a153
Drian <11167300+fdrian@users.noreply.github.com>
2022-10-13 01:48:54 -0400

    Update params

--- pknock.sh
+++ pknock.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 ## @fdrian - 2022
 ## Port knock script
-
+echo "Running $0"
 
 if [ -z $1 ];then
 	echo "No parameter passed!"
 elif [ $1 == "open" ];then
 	echo "Opening SSH port..."
-	for x in 7000 8000 9000; do nmap -Pn --host-timeout 201 --max-retries 0 -p $x adrianofreitas; done
+	for x in $2 $3 $4; do nmap -q -Pn --host-timeout 201 --max-retries 0 -p $x drian 1>/dev/null; done
+	echo "Done..."
 elif [ $1 == "close" ];then
 	echo "Closing SSH port..."
-	for x in 9000 8000 7000; do nmap -Pn --host-timeout 201 --max-retries 0 -p $x adrianofreitas; done
+	for x in $4 $3 $2; do nmap -q -Pn --host-timeout 201 --max-retries 0 -p $x drian 1>/dev/null; done
+	echo "Done..."
 else
 	echo "Invalid parameter"
 fi
-
-
-
-
-
-
